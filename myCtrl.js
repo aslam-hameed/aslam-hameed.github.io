42e3fcb0969c839b274a84094e77b0b5b8dc4e11
aslam-hameed <aslam.shahulhameed@gmail.com>
2016-09-17 01:06:16 -0400

    names

--- myCtrl.js
+++ myCtrl.js
@@ -15,8 +15,20 @@ app.controller("myCtrl", function($scope) {
     $scope.expand = function(data) {
          data.expanded=true;
     }
-    $scope.tree = [{name: "Node", expanded:true, nodes: []}];
-/*
-    $scope.tree = [{name: "Aslam"}, {name: "Musthak"}]
-*/
+
+    $scope.tree = [{name: "Node", expanded:true, nodes: [
+    {name: "Shahul Hameed", expanded:true, nodes: [
+        {name: "Aslam", expanded:true, nodes: [
+            {name: "Haleema", expanded:true, nodes: []},
+            {name: "Mariam", expanded:true, nodes: []}
+        ]},
+        {name: "Musthak", expanded:true, nodes: []},
+        {name: "Rubeena", expanded:true, nodes: []},
+        {name: "Sharafath", expanded:true, nodes: []},
+        {name: "Ansif", expanded:true, nodes: []},
+        {name: "Zahir", expanded:true, nodes: []},
+        {name: "Ruksana", expanded:true, nodes: []}
+    {name: "Moinhi", expanded:true, nodes: }
+    ]}];
+
 });
