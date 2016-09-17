94c352905f92d8585c37bea40fe0ada885be8dff
aslam-hameed <aslam.shahulhameed@gmail.com>
2016-09-17 01:09:49 -0400

    names

--- myCtrl.js
+++ myCtrl.js
@@ -17,18 +17,19 @@ app.controller("myCtrl", function($scope) {
     }
 
     $scope.tree = [{name: "Node", expanded:true, nodes: [
-    {name: "Shahul Hameed", expanded:true, nodes: [
-        {name: "Aslam", expanded:true, nodes: [
-            {name: "Haleema", expanded:true, nodes: []},
-            {name: "Mariam", expanded:true, nodes: []}
+        {name: "Shahul Hameed", expanded:true, nodes: [
+            {name: "Aslam", expanded:true, nodes: [
+                {name: "Haleema", expanded:true, nodes: []},
+                {name: "Mariam", expanded:true, nodes: []}
+            ]},
+            {name: "Musthak", expanded:true, nodes: []},
+            {name: "Rubeena", expanded:true, nodes: []},
+            {name: "Sharafath", expanded:true, nodes: []},
+            {name: "Ansif", expanded:true, nodes: []},
+            {name: "Zahir", expanded:true, nodes: []},
+            {name: "Ruksana", expanded:true, nodes: []}
         ]},
-        {name: "Musthak", expanded:true, nodes: []},
-        {name: "Rubeena", expanded:true, nodes: []},
-        {name: "Sharafath", expanded:true, nodes: []},
-        {name: "Ansif", expanded:true, nodes: []},
-        {name: "Zahir", expanded:true, nodes: []},
-        {name: "Ruksana", expanded:true, nodes: []}
-    {name: "Moinhi", expanded:true, nodes: }
-    ]}];
+        {name: "Moinhi", expanded:true, nodes: []}
+        ]}];
 
 });
